2570254038883b991d34c7fc6e3725b116fcc3ec
Nguyen-Duy-Phuoc <64922816+Nguyen-Duy-Phuoc@users.noreply.github.com>
2021-11-30 10:56:36 +0700

    Add files via upload

--- filter-tags.js
+++ filter-tags.js
@@ -5,18 +5,18 @@
     var tagged = {};
 
     $imgs.each(function () {
-    var img = this;
-    var tags = $(this).data('tags');
+        var img = this;
+        var tags = $(this).data('tags');
 
-    if (tags) {
-    tags.split(',').forEach(function (tagName) {
-        if (tagged[tagName] == null) {
-            tagged[tagName] = [];
+        if (tags) {
+            tags.split(',').forEach(function (tagName) {
+                if (tagged[tagName] == null) {
+                    tagged[tagName] = [];
+                }
+                tagged[tagName].push(img);
+            });
         }
-        tagged[tagName].push(img);
-        });
-    }
-});
+    });
 
     $('<button/>', {
         text: 'Show All',
